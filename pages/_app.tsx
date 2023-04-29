fae39fe37e24decfdc102da3f310b9b9f94e71a9
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-04-29 13:06:41 -0600

    Configuracion del tema

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
+import { darkTheme } from '</themes>'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  return (
+    <ThemeProvider theme={ darkTheme }>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
