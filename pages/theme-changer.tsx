fe46a901fa878c8070ee7a3f0038e62bb1a80ebd
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-05-05 15:08:20 -0600

    Creando theme-changer y estilos

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,11 +1,34 @@
+import { ChangeEvent, useState } from 'react'
 import { Layout } from '</components/layouts>'
-import React from 'react'
+import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
 
 const ThemeChangerChange = () => {
+
+  const [currentTheme, setCurrentTheme] = useState('Light');
+
+  const onThemeChange = ( event: ChangeEvent<HTMLInputElement> ) => {
+    setCurrentTheme( event.target.value )
+  }
+
   return (
     <Layout title='Theme Changer'>
-      
-      
+
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>Tmea</FormLabel>
+            <RadioGroup
+              value={currentTheme}
+              onChange={onThemeChange}
+            >
+              <FormControlLabel value='Light' control={<Radio/>} label='Light'/>
+              <FormControlLabel value='Dark' control={<Radio/>} label='Dark'/>
+              <FormControlLabel value='Custom' control={<Radio/>} label='Custom'/>
+            </RadioGroup>
+          </FormControl>
+        </CardContent>
+      </Card>
+
     </Layout>
     
   )
